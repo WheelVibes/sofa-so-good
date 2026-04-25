a59d53e6f8e87369ced89cdc67c17e61fcfa95b4
tzelynn <khotzelynn@gmail.com>
2026-04-25 21:36:02 +0800

    Configure Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-neutral-600">
+    <div className="min-h-screen flex items-center justify-center bg-neutral-50 text-neutral-700">
       sofa-so-good — initializing
     </div>
   );
